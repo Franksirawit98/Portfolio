d0a561441bc21ccb18277f83bda6d32d5e200016
Franksirawit98 <frank.nco21@gmail.com>
2025-03-12 07:51:35 +0700

    แก้ไขไฟล์ App.jsx รอบที่2

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import About from './Components/About/About'
 import Services from './Components/Services/Services'
 import Mywork from './Components/Mywork/Mywork'
 import Contact from './Components/Contact/Contact'
-import Footer from './Components/footer/footer'
+import Footer from './Components/Footer/Footer'
+
 
 
 
